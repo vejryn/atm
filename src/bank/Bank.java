423dea0ccd36cebffec15d9c325989ec2cc365db
Vejrynsh <Vejrynsh@169.254.153.243>
2017-02-09 08:36:49 +0700

    Finish

---
+++ src/bank/Bank.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+/**
+ *
+ * @author Vejrynsh
+ */
+public class Bank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String str, str1;
+        int choice,z,pin=1234,counter=0,a=3;
+        int t,s,tab=50000;
+    }
+    
+}
